151003f95b915cb16914502f4322a8537ab867ac
Xialan <xialan2817@gmail.com>
2020-07-04 18:38:18 -0500

    Updates

---
+++ precache-manifest.77b9e96252453e0e26c81308daf70dbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b16fa75cf24702daf2d8867a1491f48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c038aad6e754c87eed72",
+    "url": "/static/css/main.eb47a543.chunk.css"
+  },
+  {
+    "revision": "d0b287fe059d05c68c65",
+    "url": "/static/js/2.03f44038.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.03f44038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c038aad6e754c87eed72",
+    "url": "/static/js/main.6c5da8de.chunk.js"
+  },
+  {
+    "revision": "f25ff1427153577d539b",
+    "url": "/static/js/runtime-main.1a518a63.js"
+  }
+]);
